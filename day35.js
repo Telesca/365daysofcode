9526064f5f6954e11478e36abba283b0afcc2c96
Jenna Telesca <jenna.telesca@gmail.com>
2014-01-09 01:05:26 -0500

    Exercises for Day35

---
+++ day35.js
@@ -0,0 +1,19 @@
+//Exercises from Codecademy
+
+// You are now declaring an array.
+// Arrays are an awesome data structure!
+var junk = ["file", "glare", 24, 7];
+console.log(junk);
+
+// Array practice. (Array made by Codecademy. I am allergic to peanuts.)
+
+var junkData = ["Eddie Murphy", 49, "peanuts", 31];
+console.log(junkData[3])
+
+//Arrays and literature
+
+var books = ["Moby Dick", "Ulysses", "The Jungle", "Death Comes for the Arch Bishop"];
+
+for (var i = 0; i < books.length; i++) {
+    console.log("I would like to read " + books[i]);
+}
