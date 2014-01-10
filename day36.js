51025d61db4e610de383ac3bc755c5544b34a7e9
Jenna Telesca <jenna.telesca@gmail.com>
2014-01-10 00:10:43 -0500

    Day 36 array from codecademy

---
+++ day36.js
@@ -0,0 +1,9 @@
+// Ask a question on the Q&A Forum if you get stuck!
+
+var raptorLikes = ["christmas trees", "fancy cheese", "yogurt", "expensive cat food", "naps"];
+
+for (var i = 0; i < raptorLikes.length; i++) {
+    console.log("Our cat Raptor likes " + raptorLikes[i]);
+}
+
+
