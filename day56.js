e8a18f41055fc2803c3068fedc1119a615c8e55f
Jenna Telesca <jenna.telesca@gmail.com>
2014-01-30 23:43:58 -0500

    fixed the if statement

--- day56.js
+++ day56.js
@@ -2,28 +2,27 @@ laying = true;
 
 var youHit = Math.random();
 
-var damageThisRound = Math.floor(Math.random() * 5 + 1)
+var damageThisRound = Math.floor(Math.random() * 5 + 1);
 
-var totalDamage = 0
+var totalDamage = 0;
 
 while(slaying){
-    if (youHit = 1){
+    if (youHit >= .5){
         console.log("You hit the dragon!");
         totalDamage += damageThisRound;
-        if(totalDamage >= 4){
-            console.log("You slayed the dragon!");
-            slaying = false;
+            if(totalDamage >= 4){
+                console.log("You slayed the dragon!");
+         slaying = false;
         }
-        else{
-            youHit = Math.random();
+            else{
+                youHit = Math.random();
         }
     }
     else {
         console.log("The dragon defeated you!");
+       slaying = false;
     }
     
-    slaying = false;
+
     
 }
-
-
