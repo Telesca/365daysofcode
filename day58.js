c8b9862665892ed8b7fbb315ebc36ccff7feb441
Jenna Telesca <jenna.telesca@gmail.com>
2014-01-31 23:37:47 -0500

    Another drill

---
+++ day58.js
@@ -0,0 +1,17 @@
+//exercise
+
+var isEven = function(number) {
+    
+   if (isNaN(number) === true){
+        return "That's not a number, man.";
+    }   
+   else if (number%2===0){
+        return true;
+  }
+   else{
+        
+        return false;    
+    }
+  
+  
+};
